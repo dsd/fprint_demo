0ad1cf033f0193d067447f0a6748d50ca8b05432
Bastien Nocera <hadess@hadess.net>
2018-10-08 16:31:56 +0200

    build: Fix out-of-tree autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+pushd $srcdir
+
 aclocal || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
-./configure --enable-maintainer-mode $*
+
+popd
+
+if test -z "$NOCONFIGURE"; then
+	$srcdir/configure --enable-maintainer-mode $*
+fi
